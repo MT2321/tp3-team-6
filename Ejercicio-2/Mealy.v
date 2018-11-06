bb61409ab12a5ff1577d6abdbe31e5a0b101ebcd
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2018-11-06 17:45:45 -0300

    Base Terminada

--- Ejercicio-2/Mealy.v
+++ Ejercicio-2/Mealy.v
@@ -4,8 +4,7 @@ output z;
 reg[2:1] y,Y;
 //Estates of the FSM
 parameter [2:1] Default=2'b00, A=2'b01,B=2'b10,C=2'b11;
-//Next state combinational circuit
+//Next state combinational circuit and output combinational circuits
 
-//Define output
 
 endmodule
