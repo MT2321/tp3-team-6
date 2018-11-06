c772f472110be9b0145e645a1a830ec4bf3ecaf1
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2018-11-06 17:26:02 -0300

    Base de la simulacion en verilog

---
+++ Ejercicio-2/Mealy.v
@@ -0,0 +1,11 @@
+module Mealy (Clock,z,w):
+input Clock,w;
+output z;
+reg[2:1] y,Y;
+//Estates of the FSM
+parameter [2:1] Default=2'b00, A=2'b01,B=2'b10,C=2'b11;
+//Next state combinational circuit
+
+//Define output
+
+endmodule
---
+++ Ejercicio-2/Moore.v
@@ -0,0 +1,11 @@
+module Moore (Clock,z,w):
+input Clock,w;
+output z;
+reg[3:1] y,Y;
+//Estates of the FSM
+parameter [3:1] Default=3'b000, A=3'b001,B=3'b010,C=3'b011,D=3'b100;
+//Next state combinational circuit
+
+//Define output
+
+endmodule
