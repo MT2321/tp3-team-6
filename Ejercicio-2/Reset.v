4c69b28766c9fd3807c96c96933bee96fc5acb0c
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2018-11-07 18:35:25 -0300

    Señal de Reseteo

---
+++ Ejercicio-2/Reset.v
@@ -0,0 +1,20 @@
+//Reset
+`timescale 100us / 100us
+//El primer valor indica la cantidad que espera el modulo dado que
+// se lo llama #1 y la segunda indica que no va a haber un tiempo
+// de espera menor a el.
+module reset_gen(reset);
+
+parameter PERIOD = 10;
+
+output reg reset;
+	 
+    initial
+    begin
+        reset = 1'b1;
+        #(PERIOD/2);
+        reset <= 1'b0;
+        #(PERIOD/2);
+    end
+
+endmodule
